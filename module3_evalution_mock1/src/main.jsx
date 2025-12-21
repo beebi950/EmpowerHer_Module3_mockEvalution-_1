cdf20ce6a04eba41874be9bf5e8eb1d8ea9a28c4
beebi-k <kalagotlabeebi95@gmail.com>
2025-12-21 19:29:56 +0530

    Mock Evalution 1

--- module3_evalution_mock1/src/main.jsx
+++ module3_evalution_mock1/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { PostProvider } from './context/PostsContext';
+import { PostProvider } from './context/PostContext';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
 
